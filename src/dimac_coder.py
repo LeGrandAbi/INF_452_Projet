b7c6229ec7fb841927992d485c5800c0c20dc959
Omarlaa <omar.laachiri@etu.univ-grenoble-alpes.fr>
2026-04-26 12:38:02 +0200

    Add DIMAC encoding logic to encode_dimac function
    Implement DIMAC encoding for a given matrix.

--- src/dimac_coder.py
+++ src/dimac_coder.py
@@ -1,9 +1,20 @@
 import sys
 
 def encode_dimac(matrice, input_filepath):
-	'''
-	needs to be filled
-	'''
+	clauses = []
+    var_map = {}
+    n = len(matrice)
+    var_id = 1
+    for i in range(n):
+        for j in range(n):
+            var_map[(i, j)] = var_id
+            var_id += 1
+    for i in range(n):
+        for j in range(n):
+            if matrice[i][j] == 1:  # white
+                clauses.append([var_map[(i, j)]])
+            elif matrice[i][j] == -1:  # black
+                clauses.append([-var_map[(i, j)]])
 	with open(input_filepath, "w") as f:
 		f.write("p cnf 6 3\n")
 		f.write("1 -5 6 4 0\n")
