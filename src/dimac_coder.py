b6cf183691cdb53352f54af403d8374eb1bfee3a
LeGrandAbi <arsene.brosse9502@gmail.com>
2026-04-27 18:12:22 +0200

    Put what we did this afternoon

--- src/dimac_coder.py
+++ src/dimac_coder.py
@@ -1,11 +1,24 @@
 import sys
 
 def encode_dimac(matrice, input_filepath):
+	file_content = ""
+
+	# rajouter les l'entete
+	n_clauses_unit = 0 # nb de cellules =/= 0
+	n_var = len(matrice)**2
+	n_clauses = (len(matrice) -1)**2 + n_clauses_unit
+	file_content = f"p cnf {n_var} {n_clauses}\n"
+
+	# rajouter les clauses unitaires
+	
+
+	# rajouter les clauses de voisinnage
+
+
 	with open(input_filepath, "w") as f:
-		f.write("p cnf 6 3\n")
-		f.write("1 -5 6 4 0\n")
-		f.write("-1 5 3 4 0\n")
-		f.write("-3 -4 0\n")
+		f.write(file_content)
+		f.close()
+
 
 def decode_dimac(size, filepath):
 	matrice = [[0 for j in range(size)] for i in range(size)]
