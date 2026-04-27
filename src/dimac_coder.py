4aa5f2ef51f8636a1ae5e3be626e84ab87adbd35
LeGrandAbi <arsene.brosse9502@gmail.com>
2026-04-27 18:35:46 +0200

    n_clause_unit is now calculated in encode_dimac()

--- src/dimac_coder.py
+++ src/dimac_coder.py
@@ -3,8 +3,13 @@ import sys
 def encode_dimac(matrice, input_filepath):
 	file_content = ""
 
-	# rajouter les l'entete
-	n_clauses_unit = 0 # nb de cellules =/= 0
+	# rajouter l'entete
+	n_clauses_unit = 0
+	for y in range(len(matrice)):
+		for x in range(len(matrice)):
+			if matrice[y][x] != 0:
+				n_clauses_unit = n_clauses_unit + 1 
+
 	n_var = len(matrice)**2
 	n_clauses = (len(matrice) -1)**2 + n_clauses_unit
 	file_content = f"p cnf {n_var} {n_clauses}\n"
@@ -25,6 +30,6 @@ def decode_dimac(size, filepath):
 		content = f.readlines()[1]
 		f.close()
 	content = content.split()
-	
+
 	matrice = [[0 for j in range(size)] for i in range(size)]
 	return matrice
