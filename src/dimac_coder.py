1af251e6b65a1fc2ec16bd6d70f5bb5879f918ec
LeGrandAbi <arsene.brosse9502@gmail.com>
2026-04-27 13:31:41 +0200

    Removed empty comments

--- src/dimac_coder.py
+++ src/dimac_coder.py
@@ -1,9 +1,6 @@
 import sys
 
 def encode_dimac(matrice, input_filepath):
-	'''
-	needs to be filled
-	'''
 	with open(input_filepath, "w") as f:
 		f.write("p cnf 6 3\n")
 		f.write("1 -5 6 4 0\n")
@@ -11,8 +8,5 @@ def encode_dimac(matrice, input_filepath):
 		f.write("-3 -4 0\n")
 
 def decode_dimac(size, filepath):
-	'''
-	needs to be filled
-	'''
 	matrice = [[0 for j in range(size)] for i in range(size)]
 	return matrice
