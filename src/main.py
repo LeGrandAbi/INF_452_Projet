9b1ac4ae0d98c1f31352aab3b7f8aa9592e914e1
LeGrandAbi <arsene.brosse9502@gmail.com>
2026-04-29 21:29:40 +0200

    Added empty comments in main.py and moved the only function in main.py to the top of the file

--- src/main.py
+++ src/main.py
@@ -2,9 +2,6 @@ import subprocess
 from interface import *
 from dimac_coder import *
 
-DIMAC_FOLDER_PATH = "dimac"
-DIMAC_INPUT_FILENAME = "input_dimac"
-DIMAC_OUTPUT_FILENAME = "output_dimac"
 
 def execute_command(*cmd):
 	print("\nExecuting : " + str(cmd))
@@ -13,22 +10,32 @@ def execute_command(*cmd):
 	print("Output:", result.stdout)
 	print("Errors:", result.stderr)
 
-#size = int(input("Enter board size : "))
-size = 9
 
+# ...
+DIMAC_FOLDER_PATH = "dimac"
+DIMAC_INPUT_FILENAME = "input_dimac"
+DIMAC_OUTPUT_FILENAME = "output_dimac"
+
+# ...
+size = int(input("Enter board size : "))
+
+# ...
 inputInterface = InputInterface(size)
 inputInterface.run()
 matrice = inputInterface.get_matrice()
 
+# ...
 execute_command("mkdir", DIMAC_FOLDER_PATH)
 input_filepath = DIMAC_FOLDER_PATH + "/" + DIMAC_INPUT_FILENAME
 output_filepath = DIMAC_FOLDER_PATH + "/" + DIMAC_OUTPUT_FILENAME
 encode_dimac(matrice, input_filepath)
 
+# ...
 execute_command("minisat", input_filepath, output_filepath)
-#execute_command("cat", output_filepath)
 
+# ...
 result_matrice = decode_dimac(size, output_filepath)
 
+# ...
 outputInterface = OutputInterface(result_matrice)
 outputInterface.run()
